824bbbe3a78ea8d37ef8b7d75405d1954eddbbf2
Charles Knight <Charles-Knight@users.noreply.github.com>
2021-03-02 17:58:48 -0800

    Created stub for QuantumSlugAgent class with chooseAction method that just returns a random legal action

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,4 +1,15 @@
+import random
+from pacai.bin import capture
 from pacai.util import reflection
+from pacai.agents.capture.capture import CaptureAgent
+
+class QuantumSlugAgent(CaptureAgent):
+    def __init__(self, index, timeForComputing = 0.1):
+        super().__init__(index, timeForComputing)
+
+    def chooseAction(self, gameState):
+        legalActions = gameState.getLegalActions(self.index)
+        return random.choice(legalActions)
 
 def createTeam(firstIndex, secondIndex, isRed,
         first = 'pacai.agents.capture.dummy.DummyAgent',
@@ -10,8 +21,11 @@ def createTeam(firstIndex, secondIndex, isRed,
     and will be False if the blue team is being created.
     """
 
-    firstAgent = reflection.qualifiedImport(first)
-    secondAgent = reflection.qualifiedImport(second)
+    # firstAgent = reflection.qualifiedImport(first)
+    # secondAgent = reflection.qualifiedImport(second)
+
+    firstAgent = QuantumSlugAgent
+    secondAgent = QuantumSlugAgent
 
     return [
         firstAgent(firstIndex),
