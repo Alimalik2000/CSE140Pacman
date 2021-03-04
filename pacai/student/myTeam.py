372b5c6e6a5ea3e463a66762e8795718357d4bf0
Ali <ali@DESKTOP-OP9QJE3.localdomain>
2021-03-04 13:10:49 -0800

    created an Offensive and Defensive Quantum Slug agent, right not it is just a copy paste of the baseline agent

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -2,19 +2,96 @@ import random
 from pacai.bin import capture
 from pacai.util import reflection
 from pacai.agents.capture.capture import CaptureAgent
+from pacai.agents.capture.reflex import ReflexCaptureAgent
+from pacai.core.directions import Directions
+from pacai.util import counter
 
 '''
 Basic stub class that extends Capture agent
 We need to implement choose action. Currently it just
 returns a random legal action
 '''
-class QuantumSlugAgent(CaptureAgent):
-    def __init__(self, index, timeForComputing = 0.1):
-        super().__init__(index, timeForComputing)
+class OffenseQuantumSlugAgent(ReflexCaptureAgent):
+    """
+    A reflex agent that seeks food.
+    This agent will give you an idea of what an offensive agent might look like,
+    but it is by no means the best or only way to build an offensive agent.
+    """
+    def __init__(self, index, **kwargs):
+        super().__init__(index)
 
-    def chooseAction(self, gameState):
-        legalActions = gameState.getLegalActions(self.index)
-        return random.choice(legalActions)
+    def getFeatures(self, gameState, action):
+        features = counter.Counter()
+        successor = self.getSuccessor(gameState, action)
+        features['successorScore'] = self.getScore(successor)
+
+        # Compute distance to the nearest food.
+        foodList = self.getFood(successor).asList()
+
+        # This should always be True, but better safe than sorry.
+        if (len(foodList) > 0):
+            myPos = successor.getAgentState(self.index).getPosition()
+            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+            features['distanceToFood'] = minDistance
+
+        return features
+
+    def getWeights(self, gameState, action):
+        return {
+            'successorScore': 100,
+            'distanceToFood': -1
+        }
+
+
+class DefenseQuantumSlugAgent(ReflexCaptureAgent):
+    """
+    A reflex agent that tries to keep its side Pacman-free.
+    This is to give you an idea of what a defensive agent could be like.
+    It is not the best or only way to make such an agent.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index)
+
+    def getFeatures(self, gameState, action):
+        features = counter.Counter()
+        successor = self.getSuccessor(gameState, action)
+
+        myState = successor.getAgentState(self.index)
+        myPos = myState.getPosition()
+
+        # Computes whether we're on defense (1) or offense (0).
+        features['onDefense'] = 1
+        if (myState.isPacman()):
+            features['onDefense'] = 0
+
+        # Computes distance to invaders we can see.
+        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
+        invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
+        features['numInvaders'] = len(invaders)
+
+        if (len(invaders) > 0):
+            dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+            features['invaderDistance'] = min(dists)
+
+        if (action == Directions.STOP):
+            features['stop'] = 1
+
+        rev = Directions.REVERSE[gameState.getAgentState(self.index).getDirection()]
+        if (action == rev):
+            features['reverse'] = 1
+
+        return features
+
+    def getWeights(self, gameState, action):
+        return {
+            'numInvaders': -1000,
+            'onDefense': 100,
+            'invaderDistance': -10,
+            'stop': -100,
+            'reverse': -2
+        }
+
 
 def createTeam(firstIndex, secondIndex, isRed,
         first = 'pacai.agents.capture.dummy.DummyAgent',
@@ -29,8 +106,8 @@ def createTeam(firstIndex, secondIndex, isRed,
     # firstAgent = reflection.qualifiedImport(first)
     # secondAgent = reflection.qualifiedImport(second)
 
-    firstAgent = QuantumSlugAgent
-    secondAgent = QuantumSlugAgent
+    firstAgent = OffenseQuantumSlugAgent
+    secondAgent = DefenseQuantumSlugAgent
 
     return [
         firstAgent(firstIndex),
